55668279286403c395821142f320a949233a1406
JoeS51 <43276756+JoeS51@users.noreply.github.com>
2023-07-23 21:52:27 -0700

    Initial react website

---
+++ workout-website/src/App.js
@@ -0,0 +1,15 @@
+import logo from './images/3882-zyzz.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Work Out Champ</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
